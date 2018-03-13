1a28e26136afecfd4ed29a178b699e88b0cb28de
linjie.yang <linjie.yang@snapchat.com>
2018-03-13 00:29:46 +0000

    add compare script for uvos

--- compare.py
+++ compare.py
@@ -6,6 +6,7 @@ import matplotlib
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 fig, ax = plt.subplots()
+dataset = 'davis' # change to uvos if evaluate on youtube-vos
 methods=['OSVOS_parent','MSK_parent','OSVOS_M','MSK_M','OSVOS','MSK','OSMN']
 labels=['OSVOS-B','MaskTrack-B','OSVOS-M','MaskTrack-M','OSVOS','MaskTrack','Ours']
 colors =['b:', 'g:', 'b--', 'g--', 'b', 'g', 'r']
@@ -21,10 +22,13 @@ for i,m in enumerate(methods):
         max_len = max(max_len, len(seq['J']['raw'][0]))
     interp_res = []
     interp_len = 100
-    max_len -= 2
+    max_len -= 2 if dataset == 'davis' else 1
     for seq in all_res:
         interp_seq = np.zeros((interp_len)) 
-        seq = np.array(seq[1:-1])
+        if dataset == 'davis':
+            seq = np.array(seq[1:-1])
+        else:
+            seq = np.array(seq[1:])
         xp = np.arange(seq.size)
         yp = np.linspace(0,seq.size,interp_len) 
 
@@ -32,7 +36,6 @@ for i,m in enumerate(methods):
         interp_res.append(interp_seq)
     interp_res = np.array(interp_res)
     j_over_frame = np.mean(interp_res, axis=0)
-    # remove the first and the last frame
     x = np.linspace(0, 1, interp_len)
     ax.plot(x, j_over_frame, colors[i], label=labels[i])
 legend = ax.legend(loc='upper right', shadow=False)
